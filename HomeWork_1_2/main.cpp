2fee8d75641df23cebf70f85ff99badf22852c46
linapetro <lina1993_08@mail.ru>
2021-09-26 16:22:47 +0300

    Исправление ошибок

--- HomeWork_1_2/main.cpp
+++ HomeWork_1_2/main.cpp
@@ -1,9 +1,27 @@
-#include <iostream>
+//Необходимо вычислить выражения
 
-using namespace std;
-
-int main()
+#include <stdio.h>
+int main(void)
 {
-    cout << "Hello World!" << endl;
+    int a=5, b, c=5, d;
+    printf("b=");
+    scanf("%d", &b);
+    printf("d=");
+    scanf("%d", &d);
+    printf ("a=%d, b=%d, c=%d, d=%d\n", a, b, c, d);
+    a=a+b-2;
+    printf("a=%d\n", a);
+    c=c+1;
+    d=c-a+d;
+    printf("c=%d, d=%d\n", c, d);
+    a=a*c;
+    c=c-1;
+    printf("a=%d, c=%d\n", a, c);
+    a=a/10;
+    c=c/2;
+    b=b-1;
+    d=d*(c+b+a);
+    printf("a=%d, c=%d, b=%d, d=%d\n", a, c, b, d);
     return 0;
+
 }
