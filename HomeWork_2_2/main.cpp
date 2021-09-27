8cc7f40886c7a866b58b57b0d6ab7cf084b43627
linapetro <lina1993_08@mail.ru>
2021-09-27 10:42:44 +0300

    Дз 2.2

---
+++ HomeWork_2_2/main.cpp
@@ -0,0 +1,28 @@
+//Задание на определение наибольшей площади
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double R;
+    double a;
+    double d;
+    printf("Please enter R:");
+    scanf("%lf", &R);
+    printf("Please enter a:");
+    scanf("%lf", &a);
+    printf("Please enter d:");
+    scanf("%lf", &d);
+    double S_ring = M_PI * R * R;
+    double S_tr = sqrt(4) * a*a / 3;
+    double S_kv = d * d;
+
+    printf("S_ring: %f\n", S_ring);
+    printf("S_tr: %f\n", S_tr);
+    printf("S_kv: %f\n", S_kv);
+    double result = S_ring > S_tr ? S_ring: S_tr;
+    result = result > S_kv ? result : S_kv;
+    printf("Result: %f\n", result);
+    return 0;
+}
