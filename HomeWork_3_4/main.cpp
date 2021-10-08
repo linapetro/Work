40aceab0261877b6fbd8dd8d67a94da85dc0963e
linapetro <lina1993_08@mail.ru>
2021-10-08 19:52:32 +0300

    Убрана переменная binary_number

--- HomeWork_3_4/main.cpp
+++ HomeWork_3_4/main.cpp
@@ -6,12 +6,11 @@ int main()
     unsigned int N;
     printf("Please enter a number: ");
     scanf("%d", &N);
-    unsigned int binary_number = 0;
     do {
-    binary_number = (N % 2);
+    printf("%d", N % 2);
     N = N / 2;
-    printf("%d", binary_number);
     } while (N > 0);
+    printf("\n");
     return 0;
 }
 
