7f301bd19abe447072c8ef2344b459c5d7eb2e50
linapetro <lina1993_08@mail.ru>
2021-10-14 16:31:18 +0300

    Решение переделано и выполнено через sizeof.

--- HomeWork_3_4/main.cpp
+++ HomeWork_3_4/main.cpp
@@ -1,15 +1,21 @@
 //Перевод числа из десятичной системы счисления в двоичную.
 #include <iostream>
+#include <limits.h>
 
 int main()
 {
     unsigned int N;
     printf("Please enter a number: ");
     scanf("%d", &N);
-    do {
-    printf("%d", N % 2);
-    N = N / 2;
-    } while (N > 0);
+    for(int i = sizeof(N) * CHAR_BIT; i >= 0; --i)
+    {
+        if(N & (1 << i))
+        {
+            printf("1");
+        }
+        else
+            printf("0");
+    }
     printf("\n");
     return 0;
 }
