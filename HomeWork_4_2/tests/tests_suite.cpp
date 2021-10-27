c9b10b610f33d4b1bad48345f7c59dc4db4bbddc
linapetro <lina1993_08@mail.ru>
2021-10-27 09:57:04 +0300

    Добавлены тесты

---
+++ HomeWork_4_2/tests/tests_suite.cpp
@@ -0,0 +1,12 @@
+#include "gtest/gtest.h"
+#include "../DetermineTheLocation.h"
+
+TEST(HomeWork_4_2, DetermineTheLocation)
+{
+    int size1 = 5;
+    int size2 = 4;
+    int array1[size1] = {0, 1, 2, 3, 4};
+    int array2[size2] = {0, 1, 2, 3};
+    //int result = DetermineTheLocation(array1, array2, size1, size2);
+    //EXPECT_EQ(4, result);
+}
---
+++ HomeWork_4_2/tests/tests_suite.h
@@ -0,0 +1,4 @@
+#ifndef TESTS_SUITE_H
+#define TESTS_SUITE_H
+
+#endif // TESTS_SUITE_H
